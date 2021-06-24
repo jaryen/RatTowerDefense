8f0f2fca4d7559c5524d41b560cd845c32f60964
Jared Yen <jaryen@uw.edu>
2021-06-23 18:17:17 -0700

    Edited add tower button
    Added a button that will instantiate a new tower when pressed.

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public PlacementManager placementManager;
+
+    // Instantiates a new tower when the button is 
+    // pressed
+    public void CreateNewTower()
+    {
+        placementManager.StartBuilding();
+    }
+
+    // Destroys the dummy tower
+    public void ExitTowerBuilding()
+    {
+        placementManager.EndBuilding();
+    }
+}
